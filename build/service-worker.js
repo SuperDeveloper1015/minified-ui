fb43d5d3cd357cec08bd8bd6025cf37b90274039
SuperDeveloper1015 <hiccup0620@outlook.com>
2022-07-02 23:37:49 +0900

    new mainnet deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b48dc0018e2cceb18925b5315ac20794.js"
+  "./precache-manifest.59fe292223d0bb00b2b33ad4401662e4.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ build/static/js/4.9d46a46b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcyclone=this.webpackJsonpcyclone||[]).push([[4],{1928:function(c,n){}}]);
+//# sourceMappingURL=4.9d46a46b.chunk.js.map
