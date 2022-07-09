4bf20285459feb86e39297fd82d37768dd364ac4
SuperDeveloper1015 <hiccup0620@outlook.com>
2022-07-09 08:28:40 +0300

    add dramatic change

---
+++ build/precache-manifest.d4fc6ba0f83c0064e067b8fd88de0dd5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eacc1d91de1b7e0638a59e6e2762b29",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e4991bb8e6337361d52",
+    "url": "./static/css/3.6750a0ab.chunk.css"
+  },
+  {
+    "revision": "d6f9b5bc991d902326fe",
+    "url": "./static/css/main.b97f067b.chunk.css"
+  },
+  {
+    "revision": "4514e9424434be8b12c9",
+    "url": "./static/js/0.469b1994.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/0.469b1994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4991bb8e6337361d52",
+    "url": "./static/js/3.b4739bb5.chunk.js"
+  },
+  {
+    "revision": "1e2455c294b89955999fd2260c95dc2f",
+    "url": "./static/js/3.b4739bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "826561158774303bf205",
+    "url": "./static/js/4.77640127.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/4.77640127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efeb5ccb4eb795e5c7c0",
+    "url": "./static/js/5.2e041d6b.chunk.js"
+  },
+  {
+    "revision": "191c3bd33f19350a5d248013d207718d",
+    "url": "./static/js/5.2e041d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f98960e5b13008448b",
+    "url": "./static/js/6.f0af7edf.chunk.js"
+  },
+  {
+    "revision": "e7796d112bf7e8949289",
+    "url": "./static/js/7.12699e58.chunk.js"
+  },
+  {
+    "revision": "d6f9b5bc991d902326fe",
+    "url": "./static/js/main.6e30370a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.6e30370a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "552e66bcaa906de2c199",
+    "url": "./static/js/runtime-main.f4eb2694.js"
+  }
+]);
---
+++ build/static/js/7.12699e58.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcyclone=this.webpackJsonpcyclone||[]).push([[7],{1896:function(c,n){}}]);
+//# sourceMappingURL=7.12699e58.chunk.js.map
