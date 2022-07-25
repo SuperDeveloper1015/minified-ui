fca69b6966f092866f6697a2949aa58b288e2c1e
SuperDeveloper1015 <hiccup0620@outlook.com>
2022-07-25 14:44:32 +0300

    c10

---
+++ build/precache-manifest.2ed30f4d1e247611acf4b0f02040cc67.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebf5107d655660a473ae3062be4c82d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0f9bf2f3066787664c6d",
+    "url": "./static/css/2.6750a0ab.chunk.css"
+  },
+  {
+    "revision": "b225c842249ff04232e3",
+    "url": "./static/css/main.e72d8080.chunk.css"
+  },
+  {
+    "revision": "0f9bf2f3066787664c6d",
+    "url": "./static/js/2.ba47d3e3.chunk.js"
+  },
+  {
+    "revision": "67d1d58746f34ab0b417a4eda477c844",
+    "url": "./static/js/2.ba47d3e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1d6d707625eedc5184",
+    "url": "./static/js/3.f7afaa6f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/3.f7afaa6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "480b740304a3b7376395",
+    "url": "./static/js/4.d5d2e0ed.chunk.js"
+  },
+  {
+    "revision": "b225c842249ff04232e3",
+    "url": "./static/js/main.f18dc7f4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.f18dc7f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2987f6a7621a27a288",
+    "url": "./static/js/runtime-main.f3d435df.js"
+  }
+]);
